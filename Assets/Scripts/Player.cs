a6c5efb2cef83dbdbaccaaa5ecda70f1143f8863
Alexander Khristichenko <alexander.khristichenko@gmail.com>
2026-01-12 14:03:00 +0200

    added game over

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,18 +4,18 @@ public class Player : MonoBehaviour
 {
     [SerializeField] FixedJoystick joystick;
     [SerializeField] float moveSpeed;
+    [SerializeField] GameObject gameOverText;
 
     Rigidbody2D rb2d;
     float inputX, inputY;
+    int count = 0;
+    int carrotCount;
+
 
     void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
-    }
-
-    void Update()
-    {
-
+        carrotCount = GameObject.FindGameObjectsWithTag("Carrot").Length;
     }
 
     void FixedUpdate()
@@ -24,5 +24,20 @@ public class Player : MonoBehaviour
         inputY = joystick.Vertical * moveSpeed;
 
         rb2d.linearVelocity = new Vector2(inputX, inputY);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Carrot"))
+        {
+            count++;
+            Destroy(collision.gameObject);
+
+            if (count == carrotCount)
+            {
+                moveSpeed = 0f;
+                gameOverText.SetActive(true);
+            }
+        }
     }
 }
